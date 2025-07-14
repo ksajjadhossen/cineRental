2a7ba7a8850bb48f1713015110748e8d5433ebef
ksajjadhossen <ksajjadhossen07@gmail.com>
2025-07-14 16:38:33 +0600

    sidebar added on project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
-  return <Header></Header>;
+  return (
+    <>
+      <Header />
+      <main className="container grid md:grid-cols[218px_1fr] gap-3.5rem">
+        <div className="w-1/4">
+          <Sidebar />
+        </div>
+        <div className="w-3/4">{/* Main content goes here */}</div>
+      </main>
+    </>
+  );
 }
 
 export default App;
